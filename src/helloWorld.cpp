2a4b1c8bf242fa320e7eeabb4743d8fb0a6e93a2
YoonSeongjune <yoonsj7tjdwns@gmail.com>
2026-01-21 10:04:22 +0000

    update src files

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -1,5 +1,10 @@
-#include <iostream>
+#include <iostream> //표준 라이브러리, 콘솔 입출력
 using namespace std;
+
+/*
+c++ 표준 라이브러리에 있는 것들은 전부 std 라는 네임스페이스 안에 들어있다.
+앞으로 std:: 안에 있는 이름들은 그냥 이름만 써도 된다고 정하는 것
+*/
 
 int main()
 {
